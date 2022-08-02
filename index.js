34b79e1e8960562c40260beee20a33a8ca562705
adilm717 <adilm717@gmail.com>
2022-08-02 08:25:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+var http = require('http');
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello Graphql from kubernetes!, server IP is ' + (process.env.LB_IP || "localhost") ;
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
